be42a616956b5f85479bf6988d7794f5f18e6b86
Tom Huntington <thomasp.huntington@outlook.com>
2020-08-09 11:28:29 +1200

    Add project files.

---
+++ MediaCaptureBug/MediaCaptureBug/MainWindow.cpp
@@ -0,0 +1,32 @@
+﻿#include "pch.h"
+#include "MainWindow.h"
+#include "MainWindow.g.cpp"
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::MediaCaptureBug::implementation
+{
+    MainWindow::MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    int32_t MainWindow::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::myButton_Click(IInspectable const&, RoutedEventArgs const&)
+    {
+        myButton().Content(box_value(L"Clicked"));
+    }
+}
---
+++ MediaCaptureBug/MediaCaptureBug/MainWindow.h
@@ -0,0 +1,32 @@
+﻿#pragma once
+
+#pragma push_macro("GetCurrentTime")
+#undef GetCurrentTime
+
+#include "MainWindow.g.h"
+
+#pragma pop_macro("GetCurrentTime")
+
+#include "winrt/Windows.Media.Capture.h"
+
+namespace winrt::MediaCaptureBug::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+    private:
+        Windows::Media::Capture::MediaCapture m_mediaCapture{};
+    };
+}
+
+namespace winrt::MediaCaptureBug::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
